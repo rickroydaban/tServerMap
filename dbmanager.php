efcbe6f4dbc68778ca1e30f634fb15fb0ba39de2
rick royd aban <rickroydaban@gmail.com>
2013-09-12 21:18:20 -0700

    mvc

--- dbmanager.php
+++ dbmanager.php
@@ -1,4 +1,5 @@
-<?
+<?php
+ 
 if(sizeof($_GET)>0){
   //a function has been called	
   $paramsCnt=sizeof($_GET)-1; //gets the number of parameters passed
@@ -11,27 +12,73 @@ if(sizeof($_GET)>0){
   }
 	
   
-  mysql_connect("localhost", "root", "");
-  mysql_select_db("transphone");
+  mysql_connect("localhost", "586702", "4graduation");
+  mysql_select_db("586702");
   
-  if($functionname == 'getPlateNumberList'){
-    $plateNumbers = array();	
-	$getPlateNoQuery = mysql_query("SELECT * FROM taxi");
+  if($functionname == 'getCompanies'){
+  	$companies = array();
+    $query = mysql_query("SELECT * FROM company");
+  	while($row = mysql_fetch_assoc($query)){
+  		$compInfo['id'] = $row['id'];
+  		$compInfo['name'] = $row['name'];
+  		$compInfo['password'] = $row['password'];
+  		$compInfo['contact'] = $row['contact'];
+  		$compInfo['serverip'] = $row['serverip'];
 
-    while($row=mysql_fetch_array($getPlateNoQuery)){
-      $taxiInfo=array();
-	  $taxiInfo["plateNo"]=$row["plateNo"];
-	  $taxiInfo["password"]=$row["password"];
-	  $taxiInfo["bodyNo"]=$row["bodyNo"];
-	  $taxiInfo["driver"]=$row["driver"];
-	  $taxiInfo["company"]=$row["company"];
+  		array_push($companies, $compInfo);
+		}
+    echo json_encode($companies);	  
+  }
 
+  
+  if($functionname == 'getTaxiInfos'){
+  	$compID = $params[0];
+    $taxis = array();	
+    $query = mysql_query("SELECT * FROM taxi WHERE company='$compID'");
 
-	  array_push($plateNumbers,$taxiInfo);
-	}
-    echo json_encode($plateNumbers);	  
+    while($row=mysql_fetch_array($query)){
+		  $taxiInfo["plateNo"]=$row["plateNo"];
+	  	$taxiInfo["bodyNo"]=$row["bodyNo"];
+		  $taxiInfo["description"]=$row["description"];
+
+	  	array_push($taxis,$taxiInfo);
+		}
+    echo json_encode($taxis);	  
   }
   
+  if($functionname == 'getDriverInfos'){
+  	$compID = $params[0];
+    $drivers = array();	
+		$getDriverQuery = mysql_query("SELECT * FROM driver WHERE company='$compID'");
+
+    while($row=mysql_fetch_array($getDriverQuery)){
+		  $driverInfo["name"]=$row["name"];
+	  	$driverInfo["license"]=$row["license"];
+	  	$driverInfo["password"]=$row["password"];
+
+	  	array_push($drivers,$driverInfo);
+		}
+    echo json_encode($drivers);	  
+  }  
+  
+  if($functionname == 'setServerIP'){
+    $ip = $params[0];
+
+    if(mysql_query("UPDATE company SET serverip='$ip'"))
+      echo true;
+    else
+      echo mysql_error();
+  }
+
+  if($functionname == 'registerDriver'){
+		$license = $params[0];
+		$plateNo = $params[1];
+		
+		if(mysql_query("INSERT INTO taxidriver(`driver`,`plateNo`) VALUES('$license','$plateNo')"))  
+	  	echo 1;
+		else 
+	  	echo mysql_error();
+  }
 }else
   echo "no function defined";
 ?>
--- multiplemarkers.php
+++ multiplemarkers.php
@@ -2,16 +2,11 @@
 //must have session_start() before using sessions
 session_start();
 
-$zoomLevel=14;
-
-//create a session that will create an array of markers
-if(!isset($_SESSION['markers'])){
-  $_SESSION['markers']=array();
-}
+$_SESSION['zoomLevel'] = 14;
 
 //fetch all the usable data in the url passed by our java server
 if(sizeof($_GET)>0){
-  //a function has been called	
+  //a function has been called  
   $paramsCnt=sizeof($_GET)-1; //gets the number of parameters passed
   $params=array();
   //get the function name
@@ -22,20 +17,12 @@ if(sizeof($_GET)>0){
     array_push($params,$_GET['arg'.$i]);
   }
   
-  //defines the add marker function that will insert a new associative array in the marker array
-  if($functionname=='addMarker'){
-	$latLng = array();
-    $latLng['lattitude']=$params[0];
-    $latLng['longitude']=$params[1];
-	array_push($_SESSION['markers'],$latLng);  
-  }
-  
   if($functionname=='zoomIn'){
-	$zoomLevel++; 
+  $_SESSION['zoomLevel']++; 
   }
   
   if($functionname=='zoomOut'){
-	$zoomLevel--;  
+  $_SESSION['zoomLevel']--;  
   }
 } 
 ?>
@@ -49,55 +36,63 @@ if(sizeof($_GET)>0){
   </script>
 
   <script type="text/javascript">
-   	function updateMapData(){
-	  var dimens = getPreferredDimensions(); //retrieves the preferred dimension defined in the java server
-	  var width = dimens[0]; 
-	  var height = dimens[1];
+    var infowindow = new google.maps.InfoWindow();
+    var speed = 1000;
+
+    function init(){
+     var dimens = getPreferredDimensions(); //retrieves the preferred dimension defined in the java server
+     var width = dimens[0]; 
+     var height = dimens[1];
 
       //set map dimensions in accordance to the host screen dimensions
-      document.getElementById('map').style.width=width+'px';		
-	  document.getElementById('map').style.height=height+'px';		
-	}
-	
-	function test(){
-	  alert("test");	
-	}
+      document.getElementById('map').style.width=width+'px';    
+      document.getElementById('map').style.height=height+'px';    
+
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: <? echo $_SESSION['zoomLevel']; ?>,
+        disableDefaultUI: true,
+        center: new google.maps.LatLng(10.304109603060656, 123.91754111328123),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+    }
+
+    function updateMarker() {
+      if(getMarkers()!=null){
+        markers = getMarkers();
+
+      for(i=0;i<markers.length;i++){
+        markerData = markers[i].split(";");
+
+        // alert("plateNo: "+markerData[0]+" status: "+markerData[1]+" lat: "+markerData[2]+" lng: "+markerData[3]);
+        var marker = new google.maps.Marker({
+                                      map:map,
+                                      draggable:false,
+                                      position: new google.maps.LatLng(markerData[2], markerData[3])
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+
+            infowindow.setContent(
+              "<div style='width:300px;height:80px'>"+
+                "<div> plateNo: "+markerData[0]+"</div>"+
+                "<div> status: "+markerData[1]+"</div>"+
+              "</div>");
+            infowindow.open(map, marker);
+          }
+        })(marker, i));
+
+      }}
+      window.setTimeout("updateMarker();", speed);  
+    }
+
+    window.setTimeout("updateMarker();", speed);
+
+
 </script>
 </head> 
-<body onLoad="updateMapData();" marginheight="0px" marginwidth="0px">
+<body onLoad="init();" marginheight="0px" marginwidth="0px">
   <div id="map" style="width: 500px; height: 400px;">
   </div>
-
-  <script type="text/javascript">	
-  	var maxMarkers = 35;
-      
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: <? echo $zoomLevel; ?>,
-	  disableDefaultUI: true,
-      center: new google.maps.LatLng(10.304109603060656, 123.91754111328123),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker;
-
-	var jMarkers = new Array();
-	jMarkers = <? echo json_encode($_SESSION['markers']) ?>;
-
-    for(var i=0;i<jMarkers.length;i++){
-	  marker = new google.maps.Marker({
-        position: new google.maps.LatLng(jMarkers[i]['lattitude'], jMarkers[i]['longitude']),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
-  </script>
 </body>
 </html>
